8baaf924589494f88ddced53eea75eff2b4502d2
ZekLouis <louis.gaume@etu.unilim.fr>
2018-03-14 14:01:50 +0100

    Suite upload file (bug)

--- src/assets/js/controllers/AdminCtrl.js
+++ src/assets/js/controllers/AdminCtrl.js
@@ -4,19 +4,41 @@ PBA.controller('AdminCtrl', ['$scope', 'dataService', function ($scope, dataServ
     var self = this;
 
     $scope.uploadFile = function(element) {
-        //console.log(element);
-        var files = element.files;
+        console.log('coucou')
         var reader = new FileReader();
+        var files = element.files;
+        var name = '';
 
+        // event on reader load -> load "lots" in the service
         reader.onload = function() {
             Papa.parse(reader.result, {
                 header: true,
                 complete: function(results) {
-                    console.log('Parsed', results);
+                    if (results.errors.length == 0) {
+                        var new_plot = {
+                            name: name,
+                            data: results.data
+                        };
+                        dataService.addNewPlot(new_plot);
+                        $scope.$apply()
+                        console.log(self.getPlots())
+                    } else {
+                        console.log('Error during import :' + results.errors)
+                    }
                 }
             })
         };
 
-        reader.readAsText(files[0])
-    }
+        // if a file is selected -> read it
+        if (files.length > 0){
+            name = files[0].name;
+            reader.readAsText(files[0]);
+        } else {
+            console.log('file not selected');
+        }        
+    };
+
+    self.getPlots = function() {
+        return dataService.getPlots();
+    };
 }]);
--- src/assets/js/services/DataService.js
+++ src/assets/js/services/DataService.js
@@ -1,4 +1,29 @@
 'use strict';
 
 PBA.service('dataService', function(){
+    var self = this;
+    self.plots = [];
+
+    self.addNewPlot = function(new_plot) {
+        self.plots.push(new_plot);
+    };
+
+    self.getPlotsList = function() {
+        var list = [];
+        for (var plot in self.plots) {
+            list.push(self.plots[plot].name + self.plots[plot].data.length);
+        }
+        return list;
+    };
+
+    self.getPlots = function() {
+        var list = [];
+        for (var plot in self.plots) {
+            list.push({
+                name: self.plots[plot].name, 
+                length: self.plots[plot].data.length
+            });
+        }
+        return list;
+    };
 });
