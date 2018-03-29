ed6ae353b5bcfa4a0bb327ca8b0ce3f99da91a34
remsio <remi.matasse@gmail.com>
2018-03-29 22:09:02 +0200

    ajout des points sur la map

--- src/assets/js/controllers/AnalyzeCtrl.js
+++ src/assets/js/controllers/AnalyzeCtrl.js
@@ -1,11 +1,14 @@
 'use strict';
 
-PBA.controller('AnalyzeCtrl', ['$scope', 'dataService', 'filterService', function ($scope, dataService, filterService, NgMap) {
+PBA.controller('AnalyzeCtrl', ['$scope', 'dataService', 'filterService', 'mapService', function ($scope, dataService, filterService, mapService, NgMap) {
     var self = this;
     self.plots = dataService.plots;
     self.selectedPlots = filterService.getSelectedPlots();
     $scope.pieData = dataService.getPieData();
-    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-zAE8umUUIT3FSoFiFYyChRuR5YsPge0";
+
+    self.googleMapsKey = mapService.googleMapsKey;
+    $scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=" + self.googleMapsKey;
+    $scope.mapPoints = mapService.mapPoints;
 
     self.getPlotsList = function() {
         return dataService.getPlotsList();
@@ -13,6 +16,10 @@ PBA.controller('AnalyzeCtrl', ['$scope', 'dataService', 'filterService', functio
 
     self.getSelectedPlots = function () {
         return filterService.getSelectedPlots();
+    };
+
+    self.debugMap = function () {
+        mapService.updatePointsFromPlots(self.plots, NgMap);
     };
 
     self.debug = function() {
--- src/assets/js/services/MapService.js
+++ src/assets/js/services/MapService.js
@@ -2,6 +2,31 @@
 
 PBA.service('mapService', function(){
 
+    var self = this;
+    self.googleMapsKey = "AIzaSyD-zAE8umUUIT3FSoFiFYyChRuR5YsPge0";
 
+    self.mapPoints = [];
+
+    self.addPoint = function (point) {
+        self.mapPoints.push(point);
+    };
+
+    self.addPointFromAdress = function(address) {
+        $.ajax({url: "https://maps.googleapis.com/maps/api/geocode/json?address="+address+",+CA&key=" + self.googleMapsKey, success: function(result){
+                self.addPoint(result.results[0].geometry.location);
+            }});
+    };
+    
+    self.updatePointsFromPlots = function (plots, map) {
+
+        plots.forEach(function (plot) {
+
+            plot.data.forEach(function (student) {
+                self.addPointFromAdress(student['Libellé établissement']);
+            });
+        });
+        google.maps.event.trigger(map,'resize')
+
+    }
 
 });
